2b5452661e214dcee787670b7a2b058652b7bc8f
RedContritio <RedContritio@qq.com>
2020-04-15 22:40:01 +0800

    initial commit

---
+++ EasyLua/Lua.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLua
+{
+    public class Lua
+    {
+        public Lua(string script)
+        {
+            Console.WriteLine(script);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyLua;
+
+namespace Test
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Lua a = new Lua("Hello World!");
+
+            Console.ReadKey();
+            return;
+        }
+    }
+}
